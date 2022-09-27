c584ffc04622aef41951535318c6d20ca33d0949
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-09-27 18:09:46 +0000

    me pongo con el ejercicio 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#PRIMER Y SEGUNDO APARTADO
+#PRIMER Y SEGUNDO EJERCICIO
 
 num1=int(input("Escribe el primer número: "))
 print(type(num1))
@@ -12,3 +12,10 @@ print(str(num1).zfill(6))
 print("\n\n")
 
 print("{:.3f}".format(num2))
+
+
+
+
+#TERCER EJERCICIO
+
+altura= float(input("Cuanto mides?: "))
