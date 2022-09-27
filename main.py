4fc5d782b0422ffd778ba53c0188cc16ca298a3d
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-09-27 18:29:35 +0000

    ejercicio 3 terminado

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 #PRIMER Y SEGUNDO EJERCICIO
 
-num1=int(input("Escribe el primer número: "))
+num1 = int(input("Escribe el primer número: "))
 print(type(num1))
 print("\n\n")
 
-num2=float(input("Escribe el segundo número: "))
+num2 = float(input("Escribe el segundo número: "))
 print(type(num2))
 print("\n\n")
 
 print(str(num1).zfill(6))
-print("\n\n")
+print("\n")
+
 
 print("{:.3f}".format(num2))
 
-#TERCER EJERCICIO
 
-altura= float(input("Cuanto mides?: "))
+#TERCER EJERCICIO
+print("\n\n\n")
+
+altura = float(input("Cuanto mides en metros?: "))
+peso= float(input("Cúal es tu peso en kilogramos?: "))
+print("\n")
+
+print("Tu altura es {0} metros y tu peso es de {1} KG".format(altura, peso))
+
+
+
+
+
+
+
