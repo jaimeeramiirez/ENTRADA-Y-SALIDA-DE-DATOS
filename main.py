e0a3656c1507aa8641464cfb0c87584928c1aea8
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-09-27 17:37:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#PRIMER Y SEGUNDO APARTADO
+
+num1=int(input("Escribe el primer número: "))
+print(type(num1))
+print("\n\n")
+
+num2=float(input("Escribe el segundo número: "))
+print(type(num2))
+print("\n\n")
+
+print(str(num1).zfill(6))
+
+
